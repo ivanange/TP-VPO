8c93c673fd9efa83319fd8fceccbc6cd2a6c6cab
onana <onanadonald@gmail.com>
2021-06-30 14:51:04 +0100

    equalisation d'histogram et trans lineaire

--- src/test.c
+++ src/test.c
@@ -5,21 +5,42 @@
 
 int main()
 {
-    Coordinates origin = {0, 0};
-    Image *apollo = parse_image("./data/apollonian_gasket.ascii.pgm");
+    // Coordinates origin = {0, 0};
+    // Image *apollo = parse_image("./data/apollonian_gasket.ascii.pgm");
     Image *feep = parse_image("./data/feep.ascii.pgm");
-    Image *mona_lisa = parse_image("./data/mona_lisa.ascii.pgm");
-
-    // play with fleep
-    print_image(feep, &origin, feep->height - 1, feep->width - 1);
-    Hist *feep_hist = make_hist(feep, 0);
-    plot_hist(feep_hist);
-    SpatialFilter *moyenneur = create_filters();
-    // Image *feep_moyen = convulv(feep, moyenneur, ZERO_EDGES);
-    Image *feep_moyen = convulv(feep, moyenneur, PARTIAL_FILTERED_EDGES);
-    save(feep_moyen, "./data/feep_moyen_3_partial_filtering.pgm");
+    // Image *mona_lisa = parse_image("./data/mona_lisa.ascii.pgm");
+    //
+    // // play with fleep
+    // print_image(feep, &origin, feep->height - 1, feep->width - 1);
+    // Hist *feep_hist = make_hist(feep, 0);
+    // plot_hist(feep_hist);
+    // SpatialFilter *moyenneur = create_filters();
+    // // Image *feep_moyen = convulv(feep, moyenneur, ZERO_EDGES);
+    // Image *feep_moyen = convulv(feep, moyenneur, PARTIAL_FILTERED_EDGES);
+    // save(feep_moyen, "./data/feep_moyen_3_partial_filtering.pgm");
 
     // Image *sum = image_add(feep, feep);
     // Image *difference = image_sub(feep, feep);
     // Image *product = image_mul(feep, 3);
+
+    Image *fig3_brillant = parse_image("./data/sd/cacao_obscure.pgm");
+
+    // printf("%d\n", obscure->spatial_resolution);
+    // printf("%d\n", feep->tonal_resolution);
+    Coordinates coords = {0, 0};
+    Image *fig3_contrate = malloc(sizeof(Image));
+    fig3_contrate = enhance_by_histogram_equalization(fig3_brillant);
+    //
+    print_image(fig3_contrate, &coords, 23, 23);
+    // Hist *hist = make_hist(obscure, 0);
+
+    // printf("number of pixels at 193: %.2f \n", hist->hist[193]);
+
+    // print_hist(hist);
+
+    save(fig3_contrate, "./data/cacao_constrate.pgm");
+
+    // printf("pixel at 18:53 %d \n", obscure->image[18][53]);
+
+
 }
--- test/test.c
+++ test/test.c
@@ -20,6 +20,40 @@ int main()
     // printf("%d\n", r);
     return 0;
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int main()
+{
+    printf("Image Processing!\n");
+
+    Image *image = parse_image("./data/image.pgm");
+
+    Coordinates coords = {18, 50};
+
+    print_image(image, &coords, 20, 20);
+
+    printf("pixel at 18:53 %d \n", image->image[18][53]);
+
+    Hist *hist = make_hist(image, 0);
+
+    printf("number of pixels at 193: %.2f \n", hist->hist[193]);
+
+    // printf("number of pixels at 193: %d \n", hist->hist_coordinates[193].length);
+    printf("coordinates of the first pixel of shade 193: x: %d, y: %d \n", hist->hist_coordinates[193].coordinates[0].x, hist->hist_coordinates[193].coordinates[0].y);
+
+    printf("luminousity: %.2f \n", luminousity(image));
+
+    printf("contrast: %.2f \n", contrast(image));
+
+    // plot_hist(hist);
+
+    print_hist(hist);
+
+    return 0;
+}
 //
 //
 // int main(int argc, char *argv[])
@@ -82,3 +116,24 @@ int main()
 //
 //     return 0;
 // }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+#include "filters.h"
+
+int main()
+{
+    Coordinates coords = {0, 0};
+    Image *feep = parse_image("./data/feep.ascii.pgm");
+    Image *mona_lisa = parse_image("./data/mona_lisa.ascii.pgm");
+    Image *apollo = parse_image("./data/apollonian_gasket.ascii.pgm");
+    SpatialFilter *moyenneur = create_filters();
+
+    // play with fleep
+    Image *feep_moyen = convulv(feep, moyenneur, MIRROR_EDGES);
+    // print_image(feep_moyen, &coords, feep->height - 1, feep->width - 1);
+    save(feep_moyen, "./data/feep_moyen_3.pgm");
+
+    return 0;
+}
